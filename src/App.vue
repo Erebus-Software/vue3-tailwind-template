28e5bd7ebde60721faf9e584ba9adfca5fba96c8
notCan <devecann@gmail.com>
2023-07-11 13:10:56 +0300

    init files

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <div class="flex justify-center items-center">
+    <span class="font-bold">DENEME</span>
+  </div>
+</template>
---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createWebHistory, createRouter } from "vue-router"
+
+const routes = [
+  // {
+  //   path: "/",
+  //   name: "",
+  //   component: ,
+  // },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
